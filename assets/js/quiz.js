93e00a344ee0e201d98c6ec7d7a18557aaaca6a2
ShravanSalvi <shravanrakeshsalvi@gmail.com>
2026-01-17 09:25:18 +0530

    fixed the issues

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -1,9 +1,7 @@
 
-import { initializeApp } from
-"https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 
-import { getDatabase, ref, get } from
-"https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js";
+import { getDatabase, ref, get } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js";
 
 // 🔹 Firebase Config (same as upload)
 const firebaseConfig = {
