a9e10061b8d6e66b91bfd7b783a90be67afa779c
shravan salvi <shravanrakeshsalvi@gmail.com>
2026-01-17 09:26:40 +0530

    Remove Firebase API configuration from firebase.js
    Removed sensitive Firebase configuration details.

--- assets/js/firebase.js
+++ assets/js/firebase.js
@@ -8,14 +8,7 @@ import { getDatabase } from
 "https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAiWIIPynr1ro_J16GKWOKQCIuQ8jAvy6c",
-  authDomain: "quiz-app-c39c3.firebaseapp.com",
-  databaseURL: "https://quiz-app-c39c3-default-rtdb.firebaseio.com",
- projectId: "quiz-app-c39c3",
-  storageBucket: "quiz-app-c39c3.firebasestorage.app",
-  messagingSenderId: "139212464622",
-  appId: "1:139212464622:web:6eb6532d5674e41a75aeff",
-  measurementId: "G-9BCXQKS7HW"
+  //API config
 };
 
 const app = initializeApp(firebaseConfig);
